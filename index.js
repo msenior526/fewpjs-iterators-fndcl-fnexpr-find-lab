6ba6f753680596aa805578b4a7c7817702f3f51b
msenior526 <msenior526@gmail.com>
2021-05-19 19:58:56 -0400

    commit tree 9cdd5f7030f8990c9ef7bdbf94e25507f0dbd0e1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+const superbowlWin = (record) => {
+    let win = record.find(element => 
+        element.result === "W"
+    )
+    if (win == true) {
+        return win.year
+    } else {
+        return undefined
+    }
+}
